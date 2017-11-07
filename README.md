cfc69396f513aa39138ff78b443e34d5f1108fcf
Birk Johansson <birk@dhis2.org>
2017-11-07 03:04:46 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# OctoPrint-Themeify
+
+Change the looks of OctoPrint!
+Currently supports a beautiful Dark Theme inspired by Discord's color palette, more configuration and themes are planned.
+
+## Setup
+
+Install via the bundled [Plugin Manager](https://github.com/foosel/OctoPrint/wiki/Plugin:-Plugin-Manager)
+or manually using this URL:
+
+    https://github.com/birkbjo/OctoPrint-Themeify/archive/master.zip
---
+++ octoprint_themeify/__init__.py
@@ -0,0 +1,23 @@
+# coding=utf-8
+from __future__ import absolute_import
+import octoprint.plugin
+import time
+
+class ThemeifyPlugin(octoprint.plugin.StartupPlugin, 
+    octoprint.plugin.AssetPlugin,
+    octoprint.plugin.SettingsPlugin,
+    octoprint.plugin.TemplatePlugin):
+    
+    def on_after_startup(self):
+        print "Themeify initialized."
+
+    def get_assets(self):
+	    return dict(
+           less=[
+           "less/themeify.less",
+           "less/variables.less"],
+            js=["js/themeify.js"]
+	)
+
+__plugin_name__ = "Themeify"
+__plugin_implementation__ = ThemeifyPlugin()
